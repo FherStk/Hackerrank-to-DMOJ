8a1cb8e64a53664c6a94a54de1ef1cc2280c7291
FherStk <porrino.fernando@gmail.com>
2022-12-10 15:25:56 +0100

    building import

--- dmoj-import.py
+++ dmoj-import.py
@@ -1,26 +1,25 @@
-#TODO:
-#from django.utils import timezone
-#from django.contrib.auth.models import User
-#from judge.models import Problem, Judge
 import os
+
+DMOJ_DIR = "/etc/dmoj"
+PROBLEMS_DIR = "chs"
 
 def main():
 	copyTest()	
 
 def copyTest():
-	testsDir = "chs"
-	promblemsDir = "/etc/dmoj/problems"
-
-	for problem in os.listdir(testsDir):
-		problemSource = os.path.join(testsDir, problem)
-		problemDest = os.path.join(promblemsDir, problem)
+	for problem in os.listdir(PROBLEMS_DIR):
+		problemSource = os.path.join(PROBLEMS_DIR, problem)
+		problemDest = os.path.join(DMOJ_DIR, "problems", problem)
 
 		#creating the dmoj problem folder
 		os.system(f"mkdir -p {problemDest}")
 
 		for file in os.listdir(problemSource):
 			if file.endswith(".zip") or file.endswith(".yaml"):
-				os.system(f"cp {os.path.join(problemSource, file)} {os.path.join(problemDest, file)}")			
+				os.system(f"cp {os.path.join(problemSource, file)} {os.path.join(problemDest, file)}")
+
+			if file.endswith(".py"):
+				os.system(f". {DMOJ_DIR}/dmojsite/bin/activate && python3 {DMOJ_DIR}/site/manage.py shell < {os.path.join(problemSource, file)}")				
 
 if __name__ == "__main__":
 	main()
